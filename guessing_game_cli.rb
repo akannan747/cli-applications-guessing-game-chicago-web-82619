07ce73c01978bfe568b15cb85923881c7772de81
Ashwin Kannan <ashwin.kannan@berkeley.edu>
2019-08-24 03:36:10 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,10 +3,10 @@ def run_guessing_game
   num = rand(6) + 1
   puts "Guess a number between 1 and 6!"
   input = gets.chomp 
-  if input == "#{num}"
-    puts "You guessed the correct number!"
-  elsif input == "exit"
+  if input == "exit"
     puts "Goodbye!"
+  elsif input == "#{num}"
+    puts "You guessed the correct number!"
   elsif input != "#{num}"
     puts "Sorry! The computer guessed #{num}."
   end
